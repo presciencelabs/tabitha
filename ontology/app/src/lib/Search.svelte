528525915e8c0d2c0c4236b3a67f977030152a60
billy clark <billy_clark@sil.org>
2023-09-22 18:55:52 -0400

    put together the basic shell of the app

---
+++ ontology/app/src/lib/Search.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { onMount } from 'svelte'
+
+	export let autofocus = false
+
+	/** @type HTMLInputElement */
+	let input
+
+	onMount(autofocusIfRequested)
+
+	function autofocusIfRequested() {
+		autofocus && input?.focus()
+	}
+</script>
+
+<form>
+	<input type="search" name="search" placeholder="Search" bind:this={input} />
+</form>
+
+<style>
+	form {
+		padding-inline-start: calc(var(--spacing) * 2);
+		width: 100%;
+
+		display: flex;
+		align-items: center;
+	}
+
+	form > input {
+		margin-bottom: 0;
+	}
+</style>
