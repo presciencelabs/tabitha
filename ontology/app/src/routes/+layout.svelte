59a63b4ed66bf00dcbe2c052f4a744f59f5b2d2e
billy clark <billy_clark@sil.org>
2023-09-22 18:55:52 -0400

    put together the basic shell of the app

---
+++ ontology/app/src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+	import '@picocss/pico'
+	import Search from '$lib/Search.svelte'
+
+</script>
+
+<header class="container">
+	<hgroup>
+		<h1>Ontology</h1>
+		<h2>TaBiThA</h2>
+	</hgroup>
+
+	<Search autofocus />
+</header>
+
+<main class="container">
+	<slot></slot>
+</main>
+
+<footer class="container">
+	<a href="https://btn.social">Administer (TODO)</a>
+</footer>
+
+<style>
+	header {
+		display: flex;
+	}
+
+	footer {
+		display: flex;
+		justify-content: flex-end;
+	}
+</style>
