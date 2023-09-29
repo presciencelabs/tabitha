a404c0733b9e1b94a9e808a93dd9878cbb092a40
billy clark <billy_clark@sil.org>
2023-09-29 16:56:50 -0400

    refactored db interaction to mimic real calls

--- ontology/app/src/routes/+page.js
+++ ontology/app/src/routes/+page.js
@@ -1,19 +1,10 @@
-import { ontology as temp_ontology } from '$data/ontology'
+import { get_concepts } from '$data/ontology'
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ url: { searchParams } }) {
 	const query = searchParams.get('query')
 	
-	/** @type {import('$data/ontology').Concept[]} */
-	let matches
-
-	switch (query) {
-		case ''	: matches = temp_ontology	; break;
-		case null: matches = []					; break;
-		default	: matches = temp_ontology.filter(concept => concept.stem.includes(query))
-	}
-
 	return {
-		matches,
+		matches: query === null ? [] : get_concepts(query),
 	}
 }
