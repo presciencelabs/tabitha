f0bea8da3546093419eb24faba12a101ae21fe8d
billy clark <billy_clark@sil.org>
2023-10-06 18:39:52 -0400

    a little refactoring to begin cleaning the HTML up

---
+++ ontology/app/src/lib/Stat.svelte
@@ -0,0 +1,21 @@
+<script>
+	/**
+	 * @typedef StatInput
+	 *
+	 * @property {string} 			description
+	 * @property {string|number} 	value
+	 **/
+
+	/** @type {StatInput[]} */
+	export let stats
+</script>
+
+<!-- https://daisyui.com/components/stat -->
+<dl class="stats bg-base-200">
+	{#each stats as stat}
+		<div class="stat place-items-center">
+			<dd class="stat-value">{stat.value}</dd>
+			<dt class="stat-desc">{stat.description}</dt>
+		</div>
+	{/each}
+</dl>
