821d7723bd2399fbe329e480d63a95937d761908
billy clark <billy_clark@sil.org>
2023-10-07 16:22:51 -0400

    DRY'd code a bit

---
+++ ontology/app/src/lib/Details.svelte
@@ -0,0 +1,14 @@
+<script>
+	/** @type {string} must be a class name */
+	export let bgColorClass = $$props['bg-color-class'] || '' // had to use $$props because of https://github.com/sveltejs/svelte/issues/3852
+</script>
+
+<details class={`collapse collapse-arrow ${bgColorClass}`}>
+	<summary class="collapse-title">
+		<slot name="summary" />
+	</summary>
+
+	<p class="collapse-content">
+		<slot />
+	</p>
+</details>
