aaf8c5346fc54dfd0ced7d60b031a241dffde597
billy clark <billy_clark@sil.org>
2023-10-13 12:54:59 -0400

    got minimal tests running

--- ontology/app/src/hooks.client.js
+++ ontology/app/src/hooks.client.js
@@ -1,3 +1,8 @@
 import {initialize_theme} from '$lib/theme'
 
 initialize_theme()
+
+/** @type {import('@sveltejs/kit').HandleClientError} */
+export async function handleError({error, event}) {
+	console.error({error, event})
+}
--- ontology/app/tests/test.js
+++ ontology/app/tests/test.js
@@ -1,6 +1,8 @@
 import {expect, test} from '@playwright/test'
 
-test('index page has expected h1', async ({page}) => {
+test('index page has expected heading', async ({page}) => {
 	await page.goto('/')
-	await expect(page.getByRole('heading', {name: 'Welcome to SvelteKit'})).toBeVisible()
+
+	// https://playwright.dev/docs/api/class-page#page-get-by-role
+	await expect(page.getByRole('heading', {name: 'Ontology'})).toBeVisible()
 })
