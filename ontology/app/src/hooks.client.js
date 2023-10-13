dd2ee4201d5434966c7ac7cdec150ae67b6e8906
billy clark <billy_clark@sil.org>
2023-10-13 15:46:54 -0400

    added network detection, i.e., offline

--- ontology/app/src/hooks.client.js
+++ ontology/app/src/hooks.client.js
@@ -1,6 +1,9 @@
+import {initialize_detection} from '$lib/network'
 import {initialize_theme} from '$lib/theme'
 
 initialize_theme()
+
+initialize_detection()
 
 /** @type {import('@sveltejs/kit').HandleClientError} */
 export async function handleError({error, event}) {
---
+++ ontology/app/src/lib/network/Status.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Icon from '@iconify/svelte'
+	import {offline} from '.'
+
+</script>
+
+{#if $offline}
+	<span class="badge badge-error badge-lg gap-2">
+		<Icon icon="carbon:cloud-offline" /> offline
+	</span>
+{/if}
