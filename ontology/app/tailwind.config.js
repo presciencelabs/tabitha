a79232bbcd80198e9b40275210d9ba03b0ddfa92
billy clark <billy_clark@sil.org>
2023-10-25 15:05:36 -0400

    easier name on the eyes

--- ontology/app/tailwind.config.js
+++ ontology/app/tailwind.config.js
@@ -1,17 +1,21 @@
 import themes from './src/lib/theme/themes'
+import daisy_themes from 'daisyui/src/theming/themes'
+
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte}'],
 
-	plugins: [
-		require('@tailwindcss/typography'),
-		require('daisyui'),
-	],
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 
 	/** @type {import('daisyui').Config} */
 	daisyui: {
-		themes,
+		themes: [
+			...themes,
+			{
+				reformation: daisy_themes['[data-theme=halloween]'],
+			},
+		],
 		//TODO: look into this more when the time is right:  https://daisyui.com/docs/config/#rtl
 	},
 }
