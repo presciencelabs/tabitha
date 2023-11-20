ae0d7f2a9ff6ef162e7274a3967eb58d4db0ca83
billy clark <billy_clark@sil.org>
2023-11-20 17:08:57 -0500

    added new themes with daisy upgrade

--- ontology/app/src/lib/theme/themes.js
+++ ontology/app/src/lib/theme/themes.js
@@ -1,6 +1,10 @@
 // prettier-ignore
 // don't want arrays flattened by prettier
 
+// made this for a few reasons:
+// 1. excludes 'dracula'
+// 2. provides names for Selector.svelte
+// 3. allows for renaming themes, e.g., halloween to reformation
 // https://daisyui.com/docs/themes
 export default [
 	'light',
@@ -14,6 +18,7 @@ export default [
 	'corporate',
 	'cupcake',
 	'cyberpunk',
+	'dim',
 	'emerald',
 	'fantasy',
 	'forest',
@@ -22,9 +27,11 @@ export default [
 	'lofi',
 	'luxury',
 	'night',
+	'nord',
 	'pastel',
 	'reformation',
 	'retro',
+	'sunset',
 	'synthwave',
 	'valentine',
 	'winter',
