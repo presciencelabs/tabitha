a97607b62be6c199db35203ee8ef6d8714d16911
billy clark <billy_clark@presciencelabs.org>
2023-12-02 16:22:05 -0500

    migrated away from monorepo
    ontology moved to https://github.com/presciencelabs/tabitha-ontology

--- ontology/app/src/lib/Level.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	/** @type {Concept['level']} */
-	export let level
-</script>
-
-<span class="badge badge-primary badge-lg">
-	L{level}
-</span>
--- ontology/app/src/lib/Occurrences.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	/** @type {Concept['occurrences']} */
-	export let occurrences
-</script>
-
-<span class="badge badge-success badge-md tooltip tooltip-right font-mono" data-tip="Occurrences">
-	{occurrences}
-</span>
