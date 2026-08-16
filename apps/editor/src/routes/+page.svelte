ab30bbd8d53c7d46918e0536e8f99c9a72ce6eb8
billy clark <billy.clark@presciencelabs.org>
2026-08-16 14:18:39 -0400

    chore(ci): restore clean codebase and enable full CI workflow with non-blocking warnings

--- apps/editor/src/routes/+page.svelte
+++ apps/editor/src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	<<<< INTENTIONAL_BLOCKING_BUILD_ERROR >>>>
 	import CopyButton from '$lib/CopyButton.svelte'
 	import { SaveButton, save_state } from '$lib/save'
 	import { Tokens } from '$lib/tokens'
